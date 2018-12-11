7282d65945030c331638658cfe7643cfa2a2ced5
ATSISTEMAS\aobel <aobel@atsistemas.com>
2018-12-11 13:40:46 +0100

    Entidad Libro

---
+++ src/main/java/com/test/data/LibroBean.java
@@ -0,0 +1,81 @@
+package com.test.data;
+
+import com.googlecode.objectify.annotation.Cache;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+import com.wordnik.swagger.annotations.ApiModel;
+import com.wordnik.swagger.annotations.ApiModelProperty;
+
+/**
+ * Test data bean
+ */
+@Entity
+@Cache
+@ApiModel("Test object")
+public class LibroBean {
+
+    @Id
+    @ApiModelProperty(required = true)
+    private Long id;
+
+    @ApiModelProperty(required = true)
+    private String nombre;
+
+    @ApiModelProperty(required = true)
+    @Index
+    private String autor;
+
+    @ApiModelProperty(required = true)
+    private Integer anio;
+
+    @ApiModelProperty(required = true)
+    private String genero;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public Integer getAnio() {
+		return anio;
+	}
+
+	public void setAnio(Integer anio) {
+		this.anio = anio;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	@Override
+	public String toString() {
+		return "Libro [id=" + id + ", nombre=" + nombre + ", autor=" + autor + ", anio=" + anio + ", genero="
+				+ genero + "]";
+	}
+
+}
